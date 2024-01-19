fae9fffbfcc914ea62d90626e078a53dd616ebd2
Avinash <134164909+AvinashP-1611@users.noreply.github.com>
2024-01-19 16:05:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var student = {
+  name: "John Doe"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  // Get all keys of the object
+  return Object.keys(this);
+};
+
